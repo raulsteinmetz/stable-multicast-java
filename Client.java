4bf7283213295fef05d8ce02afb63a4f1fdd7319
raulsteinmetz <rsteinmetz@inf.ufsm.br>
2024-07-14 15:42:52 -0300

    menu

--- Client.java
+++ Client.java
@@ -1,31 +1,56 @@
 import StableMulticast.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Client implements IStableMulticast {
 
     private StableMulticast stableMulticast;
+    private ArrayList<String> messageBuffer;
 
     public Client(String ip, int port) {
         this.stableMulticast = new StableMulticast(ip, port, this);
+        this.messageBuffer = new ArrayList<>();
     }
 
     public void deliver(String msg) {
-        System.out.println("Received: " + msg);
+        messageBuffer.add(msg);
     }
 
     private void menu() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("Message to be sent: ");
-            String msg = scanner.nextLine();
-            if (msg.equalsIgnoreCase("exit")) {
-                stableMulticast.leaveGroup();
-                break;
-            } else {
-                this.stableMulticast.msend(msg);
+            final String clearScreen = "\033[H\033[2J";
+            System.out.print(clearScreen);
+            System.out.println("\nMenu:");
+            System.out.println("1. Send message");
+            System.out.println("2. Read previous messages");
+            System.out.println("3. Leave");
+
+            System.out.print("Choose an option: ");
+            String option = scanner.nextLine();
+
+            switch (option) {
+                case "1":
+                    System.out.print("Message to be sent: ");
+                    String msg = scanner.nextLine();
+                    this.stableMulticast.msend(msg);
+                    break;
+                case "2":
+                    System.out.println("\nPrevious Messages:");
+                    for (String message : messageBuffer) {
+                        System.out.println(message);
+                    }
+                    System.out.print("Press enter to go back to menu: ");
+                    msg = scanner.nextLine();
+                    break;
+                case "3":
+                    stableMulticast.leaveGroup();
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Invalid option. Please try again.");
             }
         }
-        scanner.close();
     }
 
     public static void main(String[] args) {
